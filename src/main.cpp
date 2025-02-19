a9593b514e2d316175d9684cc009325720be3cd2
Sebastian Behrens <sebastian.tschen.behrens@gmail.com>
2025-02-19 19:52:06 +0100

    notworking

--- src/main.cpp
+++ src/main.cpp
@@ -5,38 +5,12 @@
 #include <tinifont.h>
 #include "OneButton.h"
 #include "WiFi.h"
+#include <ESP32WifiCLI.hpp>
+#include <ArduinoOTA.h>
 #include "esp_sntp.h"
 #include "time.h"
-#include <ESP32WifiCLI.hpp>
 #include "pin_config.h"
-#include <ArduinoOTA.h>
-
-#define PIN 16
-
-// Max is 255, 32 is a conservative value to not overload
-// a USB power supply (500mA) for 12x12 pixels.
-#define BRIGHTNESS_AVERAGE 10
-#define MIN_BRIGHTNESS 30
-#define MIN_DARK_BRIGHTNESS 10
-#define MIN_COLOR CRGB(1,0,0)
-// #define delay FastLED.delay
-
-#define MATRIX_WIDTH 9
-#define MATRIX_HEIGHT 12
-#define MATRIX_TYPE         VERTICAL_ZIGZAG_MATRIX
-#define NUMMATRIX (MATRIX_WIDTH*MATRIX_HEIGHT)
-// Define matrix width and height.
-
-#define FRAMES_PER_SECOND 30
-
-#define TIME 0
-#define FULLSCREEN 1
-#define DAY_MODE 0
-#define HOUR_MODE 1
-#define MINUTE_MODE 2
-#define CONSTANT_MODE 3
-#define AUTO_MODE 0
-#define FIXED_MODE 1
+#include "main.h"
 
 
 bool gReverseDirection = false;
@@ -57,28 +31,6 @@ u_int8_t g;
 u_int8_t b;
 
 
-const char * key_ntp_server = "kntpserver";
-const char * key_tzone = "ktzone";
-const char * key_color = "kcolor";
-const char * key_hue_offset = "khueoffset";
-
-// change these params via CLI:
-const char * default_server = "pool.ntp.org";  
-const char * default_tzone = "CET-1CEST,M3.5.0,M10.5.0/3";
-const int default_color = 0xffffff;
-const char * default_display_mode = TIME;
-
-bool wcli_setup_ready = false;
-
-static uint8_t brightness_readings[BRIGHTNESS_AVERAGE] = {0};
-static uint8_t rollingBrightnesIndex = 0;
-static uint32_t sum = 0;
-static uint8_t average_brightness = 0;
-static uint8_t displayMode = TIME;
-static uint8_t colorMode = DAY_MODE;
-static uint8_t brightnessMode = AUTO_MODE;
-static uint8_t fixed_brightness = 100;
-
 
 
 CRGB getColorForDay(struct tm *timeinfo) {
@@ -87,7 +39,7 @@ CRGB getColorForDay(struct tm *timeinfo) {
 
   // Calculate the hue value (0-255) based on the total minutes
   uint8_t hue = map(totalMinutes, 0, 1440, 0, 255);
-  
+
   // Add an offset value to the hue
   uint8_t hueOffset = wcli.getInt(key_hue_offset,0);
   hue = (hue + hueOffset) % 256;
@@ -113,7 +65,7 @@ CRGB getColorForMinute(struct tm *timeinfo) {
 
   // Calculate the hue value (0-255) based on the total minutes
   uint8_t hue = map(timeinfo->tm_sec, 0, 59, 0, 255);
-  
+
   // Add an offset value to the hue
   uint8_t hueOffset = wcli.getInt(key_hue_offset,0);
   hue = (hue + hueOffset) % 256;
@@ -140,7 +92,7 @@ CRGB getColorForHour(struct tm *timeinfo) {
 
   // Calculate the hue value (0-255) based on the total minutes
   uint8_t hue = map(totalSeconds, 0, 3599, 0, 255);
-  
+
   // Add an offset value to the hue
   uint8_t hueOffset = wcli.getInt(key_hue_offset,0);
   hue = (hue + hueOffset) % 256;
@@ -186,7 +138,7 @@ void displayFullScreen(struct tm * info) {
 void displayTime(struct tm * info) {
 
     // matrix.DrawCircle(4,6,1,getColorForMinute(info));
-    matrix.setFont(&Font3x5FixedNum);    
+    matrix.setFont(&Font3x5FixedNum);
     matrix_clear();
     CRGB color = getColor(info);
     // print hours
@@ -198,7 +150,7 @@ void displayTime(struct tm * info) {
     matrix.DrawChar(5,12,info->tm_min%10+'0',color,0,1);
     matrix_show();
 
-    
+
 }
 
 void display(struct tm * info) {
@@ -220,7 +172,7 @@ void updateTimeSettings() {
   String tzone = wcli.getString(key_tzone, default_tzone);
   Serial.printf("ntp server: \t%s\r\ntimezone: \t%s\r\n",server.c_str(),tzone.c_str());
   configTime(GMT_OFFSET_SEC, DAY_LIGHT_OFFSET_SEC, server.c_str(), NTP_SERVER2);
-  setenv("TZ", tzone.c_str(), 1);  
+  setenv("TZ", tzone.c_str(), 1);
   tzset();
 }
 
@@ -319,12 +271,11 @@ void printLocalTime(char *args, Stream *response) {
     Serial.println("No time available (yet)");
     return;
   }
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  response->println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
 }
 
 void updateBrightness() {
 
-  
     uint8_t brightness = map(analogRead(34),0,4095,MIN_DARK_BRIGHTNESS,255);
 
     // Subtract the oldest reading from the sum
@@ -347,9 +298,9 @@ void updateBrightness() {
 void rainbow_wave(uint8_t thisSpeed, uint8_t deltaHue) {     // The fill_rainbow call doesn't support brightness levels.
 // uint8_t thisHue = beatsin8(thisSpeed,0,255);                // A simple rainbow wave.
  uint8_t thisHue = beat8(thisSpeed,255);                     // A simple rainbow march.
-  
+
  fill_rainbow(matrix[0], NUMMATRIX, thisHue, deltaHue);            // Use FastLED's fill_rainbow routine.
- 
+
 }
 
 void loop() {
@@ -368,6 +319,7 @@ void loop() {
   }
   while(!wcli_setup_ready) wcli.loop(); // only for fist setup
   wcli.loop();
+  wcli.server->
   ArduinoOTA.handle();
   delay(1000 / FRAMES_PER_SECOND);
 }
@@ -377,39 +329,22 @@ void setup() {
   digitalWrite(0,LOW);
 
   Serial.begin(115200);
-  
+
   // button1.attachClick([]() { shutdown(); });
   // button2.attachClick([]() { ui_switch_page(); });
 
-
-    wcli.setCallback(new mESP32WifiCLICallbacks());
-    wcli.setSilentMode(true);
-    // CLI config  
-    wcli.add("ntpserver", &setNTPServer, "\tset NTP server. Default: pool.ntp.org");
-    wcli.add("ntpzone", &setTimeZone, "\tset TZONE. https://tinyurl.com/4s44uyzn");
-    wcli.add("time", &printLocalTime, "\t\tprint the current time");
-    wcli.add("color", &setColor, "\tset constant color");
-    wcli.add("hueoffset", &setHueOffset, "\tset color hue offset");
-    wcli.add("displaymode", &setDisplayMode, "\tset display mode. T for time, F for full screen");
-    wcli.add("colormode", &setColorMode, "\tset Color mode. D for Day, H for Hour, M for Minute and C for constant color");
-    wcli.add("brightness", &setBrightness, "\tset brightness. can be 0-255 or auto");
-
-    // NTP init
-    updateTimeSettings();
-    wcli_setup_ready = wcli.isConfigured();
-    wcli.begin("nightclock");
-    wcli.setInt(key_hue_offset,0);
+  setupWCLI();
 
     time_t now;
     char strftime_buf[64];
     struct tm timeinfo;
-    
+
 
     // FastLED.addLeds<NEOPIXEL, PIN>(matrix[0], matrix.Size()).setCorrection(0xFFC0F0);
     FastLED.addLeds<NEOPIXEL, PIN>(matrix[0], matrix.Size()).setCorrection(TypicalLEDStrip);
     // FastLED.addLeds<WS2811, PIN, GRB>(matrix[0], matrix.Size()).setCorrection(UncorrectedColor);
     FastLED.setBrightness(255);
-  
+
 
     FastLED.clear();
     FastLED.show();
@@ -424,7 +359,7 @@ void setup() {
     Serial.print(" ");
     Serial.println(NUMMATRIX);
 
-    esp_log_level_set(TAG, ESP_LOG_WARN); 
+    esp_log_level_set(TAG, ESP_LOG_WARN);
     esp_log_level_set("*", ESP_LOG_WARN);
 
 
